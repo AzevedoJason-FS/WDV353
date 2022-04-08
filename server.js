62f758c436d603502d7d67c9f4898707b7eee351
Jason Azevedo <jasonazevedo@Jasons-MacBook-Pro.local>
2022-04-08 11:11:14 -0400

    updated server.js. Added comment

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
     next(error);
 });
 
+//middleware modules to send Error neatly
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({
         error: {
